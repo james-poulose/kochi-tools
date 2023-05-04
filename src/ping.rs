83cf7762a4a1e0b084f12343785ee46dba319e9a
James Poulose <james.poulose@gmail.com>
2023-05-03 19:59:33 -0500

    misc

--- src/ping.rs
+++ src/ping.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 use std::{ffi::c_void, net::Ipv4Addr};
-use std::{mem, net};
+use std::{mem, net, thread};
 use win32_error::Win32Error;
 use windows::imp::GetLastError;
 use windows::Win32::Foundation::HANDLE;
@@ -29,18 +29,36 @@ pub fn ping(cli: &Cli, args: &PingArgs) {
 
     // ICMPEcho is the oldest version and is added only for testing/diagnostics.
     // call_icmp_echo(args);
-    call_icmp_echo2_ex(args);
+    //call_icmp_echo2_ex(args);
+
+    thread::scope(|scope| {
+        println!("Spawning T1");
+        let t1 = scope.spawn(|| {
+            println!("Start T1");
+            call_icmp_echo2_ex(args, "T1");
+            println!("End T1");
+        });
+        // println!("Spawning T2");
+        // let t2 = scope.spawn(|| {
+        //     println!("Start T2");
+        //     call_icmp_echo2_ex(args, "T2");
+        //     println!("End T2");
+        // });
+
+        t1.join().unwrap();
+        // t2.join().unwrap();
+    })
 }
 
-fn parse_dns_name_or_ip_into_ipv4_ip(dns_or_ip_string: &str) -> String {
+fn parse_dns_name_or_ip_into_ipv4_ip(dns_or_ip_string: &str, tid: &str) -> String {
     // Assume that this is an ip address. Let it error out in case of a DNS name.
     let (resolved_ip, dns_name) = match net::IpAddr::from_str(dns_or_ip_string) {
         Ok(ip) => {
             // This is an IP address, let's lookup it's dns name.
-            println!("Resolving '{}'", ip);
+            println!("{}:Resolving '{}'", tid, ip);
             let dns_name = match dns_lookup::lookup_addr(&ip) {
                 Ok(name) => {
-                    println!("Resolved: '{}'", name);
+                    println!("{}:Resolved: '{}'", tid, name);
                     name
                 }
                 // There is no point in handling error here, just return the IP.
@@ -56,71 +74,35 @@ fn parse_dns_name_or_ip_into_ipv4_ip(dns_or_ip_string: &str) -> String {
                     p
                 }
                 Err(err) => {
-                    println!("{}", err);
-                    panic!("dns lookup failed for '{}'", dns_or_ip_string);
+                    println!("{}:{}", tid, err);
+                    panic!("{}:dns lookup failed for '{}'", tid, dns_or_ip_string);
                 }
             };
             (ip, dns_or_ip_string.to_string())
         }
     };
-    println!("Pinging {}", dns_name);
+    println!("{}:Pinging {}", tid, dns_name);
 
     return resolved_ip.to_string();
 }
 
-fn call_icmp_echo(args: &PingArgs) {
-    //let ip: Ipv4Addr = Ipv4Addr::from_str("8.8.8.8").unwrap();
-    let ip_str = parse_dns_name_or_ip_into_ipv4_ip(&args.dest);
-    let ip: Ipv4Addr = Ipv4Addr::from_str(&ip_str).unwrap();
-
-    let mut reply_buf = vec![0u8; REPLY_BUF_SIZE];
-    let addr_u32: u32 = ip.into();
-
-    unsafe {
-        /*
-            Calculating the reply size (from MSDN https://learn.microsoft.com/en-us/windows/win32/api/icmpapi/nf-icmpapi-icmpsendecho)
-            "The allocated size, in bytes, of the reply buffer. The buffer should be large enough to hold at least one ICMP_ECHO_REPLY structure plus RequestSize bytes of data.
-            This buffer should also be large enough to also hold 8 more bytes of data (the size of an ICMP error message)."
-
-        */
-
-        let handle: windows::core::Result<IcmpHandle> = IcmpCreateFile();
-
-        let result = IcmpSendEcho::<IcmpHandle>(
-            handle.unwrap(),
-            addr_u32,
-            PING_PAYLOAD.as_ptr() as *const c_void, // request data
-            PING_PAYLOAD.len() as u16,
-            Some(&IP_OPTS),
-            reply_buf.as_mut_ptr() as *mut c_void, // reply buffer
-            REPLY_BUF_SIZE as u32,
-            TIME_OUT, // timeout (4 seconds)
-        );
-
-        // TODO: Need to figure out how to not use a 'moved' variable ('handle').
-        // IcmpCloseHandle(handle.unwrap());
-
-        handle_response(result, reply_buf);
-    }
-}
-
-fn handle_response(result: u32, reply_buf: Vec<u8>) {
-    println!("Muttu Response: {}", result);
+fn handle_response(result: u32, reply_buf: Vec<u8>, tid: &str) {
+    println!("{}:Muttu Response: {}", tid, result);
 
     if result == 0 {
         let error_code = unsafe { GetLastError() };
         let err = Win32Error::from(error_code);
-        println!("{}", err.to_string());
+        println!("{}:{}", tid, err.to_string());
     } else {
         let repl: &ICMP_ECHO_REPLY = unsafe { mem::transmute(&reply_buf[0]) };
-        println!("Reply:{:#?}", *repl);
+        println!("{}:Reply:{:#?}", tid, *repl);
         let ip_string = Ipv4Addr::from(repl.Address);
-        println!("Response from: {}", ip_string);
+        println!("{}:Response from: {}", tid, ip_string);
     }
 }
 
-fn call_icmp_echo2_ex(args: &PingArgs) {
-    let ip_str = parse_dns_name_or_ip_into_ipv4_ip(&args.dest);
+fn call_icmp_echo2_ex(args: &PingArgs, tid: &str) {
+    let ip_str = parse_dns_name_or_ip_into_ipv4_ip(&args.dest, tid);
     let ip: Ipv4Addr = Ipv4Addr::from_str(&ip_str).unwrap();
     //let ip: Ipv4Addr = Ipv4Addr::from_str("8.8.8.8").unwrap();
     let addr_u32: u32 = ip.into();
@@ -146,6 +128,42 @@ fn call_icmp_echo2_ex(args: &PingArgs) {
 
         // TODO: Usage of moved variable error.
         //windows::Win32::NetworkManagement::IpHelper::IcmpCloseHandle(handle.unwrap());
-        handle_response(result, reply_buf);
+        println!("{}: Got response", tid);
+        handle_response(result, reply_buf, tid);
     }
 }
+
+// fn call_icmp_echo(args: &PingArgs) {
+//     //let ip: Ipv4Addr = Ipv4Addr::from_str("8.8.8.8").unwrap();
+//     let ip_str = parse_dns_name_or_ip_into_ipv4_ip(&args.dest);
+//     let ip: Ipv4Addr = Ipv4Addr::from_str(&ip_str).unwrap();
+
+//     let mut reply_buf = vec![0u8; REPLY_BUF_SIZE];
+//     let addr_u32: u32 = ip.into();
+
+//     unsafe {
+//         /*
+//             Calculating the reply size (from MSDN https://learn.microsoft.com/en-us/windows/win32/api/icmpapi/nf-icmpapi-icmpsendecho)
+//             "The allocated size, in bytes, of the reply buffer. The buffer should be large enough to hold at least one ICMP_ECHO_REPLY structure plus RequestSize bytes of data.
+//             This buffer should also be large enough to also hold 8 more bytes of data (the size of an ICMP error message)."
+
+//         */
+//         let handle: windows::core::Result<IcmpHandle> = IcmpCreateFile();
+
+//         let result = IcmpSendEcho::<IcmpHandle>(
+//             handle.unwrap(),
+//             addr_u32,
+//             PING_PAYLOAD.as_ptr() as *const c_void, // request data
+//             PING_PAYLOAD.len() as u16,
+//             Some(&IP_OPTS),
+//             reply_buf.as_mut_ptr() as *mut c_void, // reply buffer
+//             REPLY_BUF_SIZE as u32,
+//             TIME_OUT, // timeout (4 seconds)
+//         );
+
+//         // TODO: Need to figure out how to not use a 'moved' variable ('handle').
+//         // IcmpCloseHandle(handle.unwrap());
+
+//         handle_response(result, reply_buf);
+//     }
+// }
